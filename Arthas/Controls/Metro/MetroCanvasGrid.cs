50fbc1b10675b01e5f07b91fb52ec51a902297a3
ONEO <1217950746@qq.com>
2017-03-02 21:09:57 +0800

    修改成一个更高效的画布网格

--- Arthas/Controls/Metro/MetroCanvasGrid.cs
+++ Arthas/Controls/Metro/MetroCanvasGrid.cs
@@ -6,31 +6,21 @@ namespace Arthas.Controls.Metro
 {
     public class MetroCanvasGrid : ContentControl
     {
-        internal static readonly DependencyProperty ViewportProperty = ElementBase.Property<MetroCanvasGrid, Rect>(nameof(ViewportProperty));
-        internal Rect Viewport { get { return (Rect)GetValue(ViewportProperty); } set { SetValue(ViewportProperty, value); } }
-
-
+        public static readonly DependencyProperty ViewportProperty = ElementBase.Property<MetroCanvasGrid, Rect>(nameof(ViewportProperty));
+        public Rect Viewport { get { return (Rect)GetValue(ViewportProperty); } set { SetValue(ViewportProperty, value); } }
 
         public static readonly DependencyProperty GridOpacityProperty = ElementBase.Property<MetroCanvasGrid, double>(nameof(GridOpacityProperty));
         public static readonly DependencyProperty GridSizeProperty = ElementBase.Property<MetroCanvasGrid, double>(nameof(GridSizeProperty));
         public static readonly DependencyProperty CornerRadiusProperty = ElementBase.Property<MetroCanvasGrid, CornerRadius>(nameof(CornerRadiusProperty));
 
         public double GridOpacity { get { return (double)GetValue(GridOpacityProperty); } set { SetValue(GridOpacityProperty, value); } }
-        public double GridSize { get { return (double)GetValue(GridSizeProperty); } set { SetValue(GridSizeProperty, value); SizeChange(); } }
         public CornerRadius CornerRadius { get { return (CornerRadius)GetValue(CornerRadiusProperty); } set { SetValue(CornerRadiusProperty, value); } }
 
         public bool IsApplyTheme { get; set; } = true;
 
-        void SizeChange()
-        {
-            Viewport = new Rect(0.0, 0.0, GridSize * 2 / ActualWidth, GridSize * 2 / ActualHeight);
-        }
-
         public MetroCanvasGrid()
         {
             Utility.Refresh(this);
-            Loaded += delegate { SizeChange(); };
-            SizeChanged += delegate { SizeChange(); };
         }
 
         static MetroCanvasGrid()
