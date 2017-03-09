2054b6f7ed54c6b145d6ae57326bda82af9ae549
ONEO <1217950746@qq.com>
2017-03-09 17:08:52 +0800

    精简代码

--- Arthas/Controls/Metro/MetroWindow.cs
+++ Arthas/Controls/Metro/MetroWindow.cs
@@ -1,20 +1,14 @@
 ﻿using Arthas.Themes;
-using Arthas.Utility.Computer;
 using Arthas.Utility.Element;
+using System;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System;
-using System.Threading;
-#if DEBUG
-using System.Diagnostics;
-#endif
 
 namespace Arthas.Controls.Metro
 {
     public class MetroWindow : Window
     {
-
         public static readonly DependencyProperty IsSubWindowShowProperty = ElementBase.Property<MetroWindow, bool>(nameof(IsSubWindowShowProperty), false);
         public static readonly DependencyProperty MenuProperty = ElementBase.Property<MetroWindow, object>(nameof(MenuProperty), null);
         public static readonly new DependencyProperty BorderBrushProperty = ElementBase.Property<MetroWindow, Brush>(nameof(BorderBrushProperty));
@@ -38,67 +32,42 @@ namespace Arthas.Controls.Metro
             ElementBase.GoToState(this, IsSubWindowShow ? "Enabled" : "Disable");
         }
 
-
         public object ReturnValue { get; set; } = null;
         public bool EscClose { get; set; } = false;
 
-        public MetroWindow()
+        protected override void OnInitialized(EventArgs e)
         {
-            Initialized += delegate
+            base.OnInitialized(e);
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            AllowsTransparency = false;
+            if (WindowStyle == WindowStyle.None)
             {
                 WindowStyle = WindowStyle.SingleBorderWindow;
-            };
+            }
+        }
 
-            Loaded += delegate
+        public MetroWindow()
+        {
+            // 修复WindowChrome导致的窗口大小错误
+            var sizeToContent = SizeToContent.Manual;
+            Loaded += (ss, ee) =>
             {
-                GoToState();
-
-                switch (SizeToContent)
-                {
-                    case SizeToContent.WidthAndHeight:
-                    case SizeToContent.Height:
-                    case SizeToContent.Width:
-                        SizeToContent = SizeToContent.Manual;
-                        Width = ActualWidth - 16;
-                        Height = ActualHeight - 32 - 7 + 3;
-
-                        /*
-                            // 解决何必DLL到EXE中之后残留的Bug
-                            if (!File.Exists(Path.StartPath + @"\Arthas.dll"))
-                            {
-                                // Height = Height + 10;
-                                // Width = Width + 10; // 可能某些情况下不需要，但是无法获取到这些情况，所以全部加10像素，一般来说不会有问题
-                            }
-                        */
-                        break;
-                    default:
-                        Height = ActualHeight + 32;
-                        break;
-                }
-
-                MinWidth = MinWidth == 0.0 ? Width : MinWidth;
-                MinHeight = MinHeight == 0.0 ? Height : MinHeight;
-                if (ResizeMode == ResizeMode.CanMinimize || ResizeMode == ResizeMode.NoResize)
-                {
-                    MaxWidth = Width;
-                    MaxHeight = Height;
-                }
-                Left = (Screen.ScreenWidthLogic - Width) / 2;
-                Top = (Screen.ScreenHeightLogic - Height) / 3;
+                sizeToContent = SizeToContent;
             };
+            ContentRendered += (ss, ee) =>
+            {
+                SizeToContent = SizeToContent.Manual;
+                Width = ActualWidth;
+                Height = ActualHeight;
+                SizeToContent = sizeToContent;
+            };
+
             KeyUp += delegate (object sender, KeyEventArgs e)
             {
                 if (e.Key == Key.Escape && EscClose)
                 {
                     Close();
-                }
-            };
-
-            Closing += delegate
-            {
-                if (BackgroundThread != null)
-                {
-                    BackgroundThread.Abort();
                 }
             };
             StateChanged += delegate
@@ -111,80 +80,12 @@ namespace Arthas.Controls.Metro
                       }
                   }
               };
-#if DEBUG
-            ContentRendered += delegate
-            {
-                if (Debugger.IsAttached)
-                {
-                    Title += "　编译器调试模式";
-                }
-                else
-                {
-                    Title += "　调试模式";
-                }
-                Debug.WriteLine("当前处于调试版本，请勿发布！！！");
-            };
-#endif
             Utility.Refresh(this);
         }
 
         static MetroWindow()
         {
             ElementBase.DefaultStyle<MetroWindow>(DefaultStyleKeyProperty);
-        }
-
-        // 以下给窗口附加一个后台线程
-
-        public Thread BackgroundThread { get; set; }
-
-        /// <summary>
-        /// 后台运行
-        /// </summary>
-        /// <param name="action">动作</param>
-        /// <param name="newThread">是否新开线程</param>
-        /// <returns></returns>
-        public Thread BackgroundRun(Action action, bool newThread = false)
-        {
-            if (newThread)
-            {
-                Thread t = new Thread(() => { action(); });
-                t.Start();
-                return t;
-            }
-            else
-            {
-                if (BackgroundThread != null)
-                {
-                    BackgroundThread.Abort();
-                }
-                BackgroundThread = new Thread(() => { action(); });
-                BackgroundThread.Start();
-                return BackgroundThread;
-            }
-        }
-
-        /// <summary>
-        /// 在指定线程运行动作
-        /// </summary>
-        /// <param name="thread"></param>
-        /// <param name="action"></param>
-        public void BackgroundRun(Thread thread, Action action)
-        {
-            if (thread != null)
-            {
-                thread.Abort();
-            }
-            thread = new Thread(() => { action(); });
-            thread.Start();
-        }
-
-        public void Sleep(int millisecondsTimeout = 1)
-        {
-            Thread.Sleep(millisecondsTimeout);
-        }
-        public void Invoke(Action action)
-        {
-            Dispatcher.Invoke(action);
         }
     }
 }
